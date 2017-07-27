c273dddd57def396798e9bcca346ca5e4f84ff1b
dblack <dblack@plangrid.com>
2017-07-27 07:50:54 -0700

    .

--- app/app.py
+++ app/app.py
@@ -7,7 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app, SQLALCHEMY_TRACK_MODIFICATIONS=False)
 
 
 class User(db.Model):
