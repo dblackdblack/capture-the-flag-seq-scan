0e8044c3bdc23b20f8bef50b466aac6351dcf417
dblack <dblack@plangrid.com>
2017-07-30 18:30:29 -0700

    oops missing space

--- cmd.sh
+++ cmd.sh
@@ -4,4 +4,4 @@ user_to_find=$(cat /user_to_find)
 echo "****** Try to find this user's name:  $user_to_find"
 service postgresql start
 cd /app
-exec gunicorn --acess-logformat='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'--access-logfile=- --bind 0.0.0.0:8000 app.app:app
+exec gunicorn --acess-logformat='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s' --access-logfile=- --bind 0.0.0.0:8000 app.app:app
