969274df0087cce1a7eef8da3e21846c35055224
dblack <dblack@plangrid.com>
2017-08-01 08:49:51 -0700

    add debug mode

--- app/app.py
+++ app/app.py
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+if os.getenv('DEBUG', '').lower() in ('1', 'true'):
+    app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
