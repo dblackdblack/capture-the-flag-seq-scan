658da412dd2066257e225a75947e28c81e1fc466
dblack <dblack@plangrid.com>
2017-08-01 06:52:44 -0700

    .

--- cmd.sh
+++ cmd.sh
@@ -2,6 +2,7 @@
 
 user_to_find=$(cat /user_to_find)
 echo "****** Try to find this user's name:  $user_to_find"
+echo "****** Visit this URL:  http://localhost:8000/user/${user_to_find}@example.com"
 service postgresql start
 cd /app
 exec gunicorn --access-logformat='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s usec' --access-logfile=- --bind 0.0.0.0:8000 app.app:app
